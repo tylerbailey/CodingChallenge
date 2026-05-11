dbeaa69f49c4eb736886cf125815208b2e9228fc
Tyler Bailey <tpbailey@uno.edu>
2026-05-11 16:52:46 -0500

    Removed old comment on IRepository, lowered DI library version to match .NET 8.0, added localpilot to gitignore

--- ShoppingCart/Repositories/Interfaces/IRepository.cs
+++ ShoppingCart/Repositories/Interfaces/IRepository.cs
@@ -1,10 +1,6 @@
 ﻿using CodingChallenge.Models.Interfaces;
 namespace CodingChallenge.Repositories.Interfaces
 {
-    /**
-     * Generic repository interface for performing basic CRUD operations on entities.
-     * It defines methods for saving, retrieving, and updating entities of type T, where T is a class that implements the IEntity interface.
-     */
     public interface IRepository<T> where T : class, IEntity
     {
         void Save(T value);
